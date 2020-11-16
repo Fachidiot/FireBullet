e6b81f2cd55b8d55112d0769c4e0a9a62e6eac7b
fachidiot <jerrykim428@gmail.com>
2020-11-16 16:55:18 +0900

    First Commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Vector3 velo;
+    public float ttl;
+    public float speed;
+
+    void Start()
+    {
+        Destroy(gameObject, ttl);    
+    }
+
+    void Update()
+    {
+        transform.position += velo * Time.deltaTime;
+    }
+
+    public void SetNormalizedDirection(Vector3 dir)
+    {
+        velo = dir * speed;
+    }
+}
---
+++ Assets/Scripts/FireBullets.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullets : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletpoint;
+    public float interval;
+    public int count;
+    public float angle;
+
+    void Start()
+    {
+        StartCoroutine("FireLoop");
+    }
+
+    IEnumerator FireLoop()
+    {
+        while (IsValidCondition())
+        {
+            float gap = count > 1 ? angle / (count - 1) : 0;
+            float startAngle = -angle / 2.0f;
+            for (int i = 0; i < count; i++)
+            {
+                float theta = startAngle + gap * i;
+                theta *= Mathf.Deg2Rad;
+                GameObject go = Instantiate(bullet, bulletpoint.position, Quaternion.identity);
+                go.GetComponent<Bullet>().SetNormalizedDirection(new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0));
+            }
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    bool IsValidCondition()
+    {
+        return interval > 0 && count > 0;
+    }
+}
